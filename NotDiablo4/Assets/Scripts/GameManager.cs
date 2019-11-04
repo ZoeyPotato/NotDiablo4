793be7e37a91b75a651137d5cdb955724e19a6b1
Zoey <zoey.roth@outlook.com>
2019-11-03 17:39:21 -0800

    initial commit

---
+++ NotDiablo4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+public class GameManager : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log("hello, is this thing working?");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+}
---
+++ NotDiablo4/Assets/TiledMaps/Tileset_0.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="world_tileset_proto" tilewidth="64" tileheight="32" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="32" source="../sprites/ground_proto.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="32" source="../sprites/wall_proto.png"/>
+  <objectgroup draworder="index">
+   <object id="2" x="31.8202" y="0.171997">
+    <polygon points="0.179766,-0.171997 -31.8202,15.8287 0.179055,31.828 32.1798,15.83"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
