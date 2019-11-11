147ae3ee8d964cbc5515cfac9a0b64a54c570209
Zoey <zoey.roth@outlook.com>
2019-11-10 20:38:24 -0800

    adding unity unit test proj removing old UnitTest proj directory adding some quick tests for WorldMap

---
+++ NotDiablo4/Assets/Scripts/UnitTests/WorldMapTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+
+namespace Tests
+{
+    public class WorldMap
+    {
+        [Test]
+        public void MapExists()
+        {
+            GameObject worldMap = GameObject.FindWithTag("WorldMap");
+
+            Assert.IsNotNull(worldMap);
+        }
+
+        [Test]
+        public void ThereIsOnlyOneMap()
+        {
+            GameObject[] worldMaps = GameObject.FindGameObjectsWithTag("WorldMap");
+
+            Assert.AreEqual(1, worldMaps.Length);
+        }
+
+        [Test]
+        public void MapIsOrigin()
+        {
+            GameObject worldMap = GameObject.FindWithTag("WorldMap");
+
+            Assert.AreEqual(Vector3.zero, worldMap.transform.position);
+        }
+    }
+}
