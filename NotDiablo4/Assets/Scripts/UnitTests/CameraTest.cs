1d6ee8688b8a5f30ace9cfc65ed29164eb5af3bc
Zoey <zoey.roth@outlook.com>
2019-11-16 17:51:26 -0800

    quick commit before travel - adding camera test class

---
+++ NotDiablo4/Assets/Scripts/UnitTests/CameraTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class CameraTest
+    {
+        [Test]
+        public void CameraExists()
+        {
+            //TODO
+            GameObject worldMap = GameObject.FindWithTag("WorldMap");
+
+            Assert.IsNotNull(worldMap);
+        }
+    }
+}
