6d7b17235e520c89ff32be969927518179d2c08d
Zoey <zoey.roth@outlook.com>
2020-06-10 21:06:21 -0700

    Implemented all the movement tests as loops inside loops. This helped simplify the testing of all movement inputs. A lot less copy/pasted code now :O

--- NotDiablo4/Assets/Scripts/UnitTests/PlayMode/PlayerTest.cs
+++ NotDiablo4/Assets/Scripts/UnitTests/PlayMode/PlayerTest.cs
@@ -36,89 +36,39 @@ namespace PlayModeUnitTests
         [UnityTest]
         public IEnumerator Movement()
         {
-            yield return movementUp();
-            yield return movementLeftUp();
-            yield return movementLeft();
-            yield return movementLeftDown();
-            yield return movementDown();
-            yield return movementRightDown();
-            yield return movementRight();
-            yield return movementRightUp();
+            int horizontalInput = -1;
+            int verticalInput   = -1;
+
+                while (horizontalInput <= 1)
+            {
+                while (verticalInput   <= 1)
+                {
+                    Vector2 initialPos = rigidBody.position;
+
+                    yield return Utility.PlayerMovement(player, horizontalInput, verticalInput);
+
+                    #region asserts
+                    if (horizontalInput == -1)
+                        Assert.Less    (rigidBody.position.x, initialPos.x);
+                    if (verticalInput   == -1)
+                        Assert.Less    (rigidBody.position.y, initialPos.y);
+                    if (horizontalInput ==  0)
+                        Assert.AreEqual(initialPos.x, rigidBody.position.x);
+                    if (verticalInput   ==  0)
+                        Assert.AreEqual(initialPos.y, rigidBody.position.y);
+                    if (horizontalInput ==  1)
+                        Assert.Greater (rigidBody.position.x, initialPos.x);
+                    if (verticalInput   ==  1)
+                        Assert.Greater (rigidBody.position.y, initialPos.y);
+                    #endregion
+
+                    verticalInput++;
+                }
+
+                horizontalInput++;
+                verticalInput = -1;
+            }
         }
-        #region movement
-        private IEnumerator movementUp()
-        {
-            Vector2 initialPos = rigidBody.position;
-
-            yield return Utility.PlayerMovement(player,  0,  1);
-
-            Assert.AreEqual(initialPos.x, rigidBody.position.x);
-            Assert.Greater (rigidBody.position.y, initialPos.y);
-        }
-        private IEnumerator movementLeftUp()
-        {
-            Vector2 initialPos = rigidBody.position;
-
-            yield return Utility.PlayerMovement(player, -1,  1);
-
-            Assert.Less    (rigidBody.position.x, initialPos.x);
-            Assert.Greater (rigidBody.position.y, initialPos.y);
-        }
-        private IEnumerator movementLeft()
-        {
-            Vector2 initialPos = rigidBody.position;
-
-            yield return Utility.PlayerMovement(player, -1,  0);
-
-            Assert.Less    (rigidBody.position.x, initialPos.x);
-            Assert.AreEqual(initialPos.y, rigidBody.position.y);
-        }
-        private IEnumerator movementLeftDown()
-        {
-            Vector2 initialPos = rigidBody.position;
-
-            yield return Utility.PlayerMovement(player, -1, -1);
-
-            Assert.Less    (rigidBody.position.x, initialPos.x);
-            Assert.Less    (rigidBody.position.y, initialPos.y);
-        }
-        private IEnumerator movementDown()
-        {
-            Vector2 initialPos = rigidBody.position;
-
-            yield return Utility.PlayerMovement(player,  0, -1);
-
-            Assert.AreEqual(initialPos.x, rigidBody.position.x);
-            Assert.Less    (rigidBody.position.y, initialPos.y);
-        }
-        private IEnumerator movementRightDown()
-        {
-            Vector2 initialPos = rigidBody.position;
-
-            yield return Utility.PlayerMovement(player,  1, -1);
-
-            Assert.Greater (rigidBody.position.x, initialPos.x);
-            Assert.Less    (rigidBody.position.y, initialPos.y);
-        }
-        private IEnumerator movementRight()
-        {
-            Vector2 initialPos = rigidBody.position;
-
-            yield return Utility.PlayerMovement(player,  1,  0);
-
-            Assert.Greater (rigidBody.position.x, initialPos.x);
-            Assert.AreEqual(initialPos.y, rigidBody.position.y);
-        }
-        private IEnumerator movementRightUp()
-        {
-            Vector2 initialPos = rigidBody.position;
-
-            yield return Utility.PlayerMovement(player,  1,  1);
-
-            Assert.Greater (rigidBody.position.x, initialPos.x);
-            Assert.Greater (rigidBody.position.y, initialPos.y);
-        }
-        #endregion
 
 
         //TODO move player state machine in its own class
@@ -142,97 +92,35 @@ namespace PlayModeUnitTests
         [Test]
         public void StateMachineGoMoving()
         {
-            stateMachineGoMovingUp();
-            stateMachineGoMovingLeftUp();
-            stateMachineGoMovingLeft();
-            stateMachineGoMovingLeftDown();
-            stateMachineGoMovingDown();
-            stateMachineGoMovingRightDown();
-            stateMachineGoMovingRight();
-            stateMachineGoMovingRightUp();
+            int horizontalInput = -1;
+            int verticalInput   = -1;
+
+                while (horizontalInput <= 1)
+            {
+                while (verticalInput   <= 1)
+                {
+                    if (horizontalInput == 0 && verticalInput == 0)
+                    {
+                        verticalInput++;
+
+                        continue;
+                    }
+
+                    PlayerInput movement     = new PlayerInput();
+                    movement.HorizontalInput = horizontalInput;
+                    movement.VerticalInput   = verticalInput;
+
+                    player.StateMachine(movement);
+
+                    Assert.AreEqual(Player.State.Moving, player.GetCurrentState());
+
+                    verticalInput++;
+                }
+
+                horizontalInput++;
+                verticalInput = -1;
+            }
         }
-        #region stateMachineGoMoving
-        private void stateMachineGoMovingUp()
-        {
-            PlayerInput moveUp     = new PlayerInput();
-            moveUp.HorizontalInput =  0;
-            moveUp.VerticalInput   =  1;
-
-            player.StateMachine(moveUp);
-
-            Assert.AreEqual(Player.State.Moving, player.GetCurrentState());
-        }
-        private void stateMachineGoMovingLeftUp()
-        {
-            PlayerInput moveLeftUp     = new PlayerInput();
-            moveLeftUp.HorizontalInput = -1;
-            moveLeftUp.VerticalInput   =  1;
-
-            player.StateMachine(moveLeftUp);
-
-            Assert.AreEqual(Player.State.Moving, player.GetCurrentState());
-        }
-        private void stateMachineGoMovingLeft()
-        {
-            PlayerInput moveLeft     = new PlayerInput();
-            moveLeft.HorizontalInput = -1;
-            moveLeft.VerticalInput   =  0;
-
-            player.StateMachine(moveLeft);
-
-            Assert.AreEqual(Player.State.Moving, player.GetCurrentState());
-        }
-        private void stateMachineGoMovingLeftDown()
-        {
-            PlayerInput moveLeftDown     = new PlayerInput();
-            moveLeftDown.HorizontalInput = -1;
-            moveLeftDown.VerticalInput   = -1;
-
-            player.StateMachine(moveLeftDown);
-
-            Assert.AreEqual(Player.State.Moving, player.GetCurrentState());
-        }
-        private void stateMachineGoMovingDown()
-        {
-            PlayerInput moveDown     = new PlayerInput();
-            moveDown.HorizontalInput =  0;
-            moveDown.VerticalInput   = -1;
-
-            player.StateMachine(moveDown);
-
-            Assert.AreEqual(Player.State.Moving, player.GetCurrentState());
-        }
-        private void stateMachineGoMovingRightDown()
-        {
-            PlayerInput moveRightDown     = new PlayerInput();
-            moveRightDown.HorizontalInput =  1;
-            moveRightDown.VerticalInput   = -1;
-
-            player.StateMachine(moveRightDown);
-
-            Assert.AreEqual(Player.State.Moving, player.GetCurrentState());
-        }
-        private void stateMachineGoMovingRight()
-        {
-            PlayerInput moveRight     = new PlayerInput();
-            moveRight.HorizontalInput =  1;
-            moveRight.VerticalInput   =  0;
-
-            player.StateMachine(moveRight);
-
-            Assert.AreEqual(Player.State.Moving, player.GetCurrentState());
-        }
-        private void stateMachineGoMovingRightUp()
-        {
-            PlayerInput moveRightUp     = new PlayerInput();
-            moveRightUp.HorizontalInput =  1;
-            moveRightUp.VerticalInput   =  1;
-
-            player.StateMachine(moveRightUp);
-
-            Assert.AreEqual(Player.State.Moving, player.GetCurrentState());
-        }
-        #endregion
 
         [Test]
         public void stateMachineGoMeleeing()
@@ -248,14 +136,28 @@ namespace PlayModeUnitTests
         [Test]
         public void stateMachineGoMeleeingWhileMoving()
         {
-            PlayerInput meleeWhileMovement     = new PlayerInput();
-            meleeWhileMovement.HorizontalInput = 1;
-            meleeWhileMovement.VerticalInput   = 1;
-            meleeWhileMovement.LeftClick       = 1;
+            int horizontalInput = -1;
+            int verticalInput   = -1;
 
-            player.StateMachine(meleeWhileMovement);
+                while (horizontalInput <= 1)
+            {
+                while (verticalInput   <= 1)
+                {
+                    PlayerInput meleeWhileMoving     = new PlayerInput();
+                    meleeWhileMoving.HorizontalInput = horizontalInput;
+                    meleeWhileMoving.VerticalInput   = verticalInput;
+                    meleeWhileMoving.LeftClick       = 1;
 
-            Assert.AreEqual(Player.State.Meleeing, player.GetCurrentState());
+                    player.StateMachine(meleeWhileMoving);
+
+                    Assert.AreEqual(Player.State.Meleeing, player.GetCurrentState());
+
+                    verticalInput++;
+                }
+
+                horizontalInput++;
+                verticalInput = -1;
+            }
         }
 
         [UnityTest]
@@ -268,56 +170,42 @@ namespace PlayModeUnitTests
 
             while (meleeTimer > 0)
             {
-                stateMachineMeleeingLockZeroInput();
-                stateMachineMeleeingLockMovementInput();
-                stateMachineMeleeingLockMeleeInput();
-                stateMachineMeleeingLockAllInput();
+                int horizontalInput = -1;
+                int verticalInput   = -1;
+                int leftClick       =  0;
+
+                        while (horizontalInput <= 1)
+                {
+                        while (verticalInput   <= 1)
+                    {
+                        while (leftClick       <= 1)
+                        {
+                            PlayerInput inputs     = new PlayerInput();
+                            inputs.HorizontalInput = horizontalInput;
+                            inputs.VerticalInput   = verticalInput;
+                            inputs.LeftClick       = leftClick;
+
+                            player.StateMachine(inputs);
+
+                            Assert.AreEqual(Player.State.Meleeing, player.GetCurrentState());
+
+                            leftClick++;
+                        }
+
+                        verticalInput++;
+                        leftClick = 0;
+                    }
+
+                    horizontalInput++;
+                    verticalInput = -1;
+                    leftClick = 0;
+                }
 
                 yield return new WaitForFixedUpdate();
 
                 meleeTimer -= Time.fixedDeltaTime;
             }
         }
-        #region stateMachineMeleeingLock
-        private void stateMachineMeleeingLockZeroInput()
-        {
-            PlayerInput input = new PlayerInput();
-
-            player.StateMachine(input);
-
-            Assert.AreEqual(Player.State.Meleeing, player.GetCurrentState());
-        }
-        private void stateMachineMeleeingLockMovementInput()
-        {
-            PlayerInput input     = new PlayerInput();
-            input.HorizontalInput = 1;
-            input.VerticalInput   = 1;
-
-            player.StateMachine(input);
-
-            Assert.AreEqual(Player.State.Meleeing, player.GetCurrentState());
-        }
-        private void stateMachineMeleeingLockMeleeInput()
-        {
-            PlayerInput input = new PlayerInput();
-            input.LeftClick   = 1;
-
-            player.StateMachine(input);
-
-            Assert.AreEqual(Player.State.Meleeing, player.GetCurrentState());
-        }
-        private void stateMachineMeleeingLockAllInput()
-        {
-            PlayerInput input     = new PlayerInput();
-            input.HorizontalInput = 1;
-            input.VerticalInput   = 1;
-            input.LeftClick       = 1;
-
-            player.StateMachine(input);
-
-            Assert.AreEqual(Player.State.Meleeing, player.GetCurrentState());
-        }
-        #endregion
 
         //TODO public IEnumerator ExitMeleeingState()
     }
